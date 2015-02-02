09f6a314184726278020ea016fea319d79080f03
ROEL CANTU <roel.cantu@yahoo.com>
2015-02-02 14:18:53 -0600

    Editing

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,5 +1,7 @@
 class Appointment < ActiveRecord::Base
     validates_presence_of :first_name, :last_name, :age,
-    :date_of_birth, :city, :state, :zip_code, :urgency, :appt_time, :note
+    :date_of_birth, :city, :state, :zip_code, :urgency, :appt_time
+
+      validates :age, numericality: { greater_than: 20 }
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150202195147) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "appointments", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.integer  "age"
+    t.date     "date_of_birth"
+    t.string   "city"
+    t.string   "state"
+    t.string   "zip_code"
+    t.string   "urgency"
+    t.datetime "appt_time"
+    t.string   "note"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
 
 end
