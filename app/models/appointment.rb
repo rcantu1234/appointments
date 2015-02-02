fe07e2c911158d135cbfedf5d2adcf8973c615ea
ROEL CANTU <roel.cantu@yahoo.com>
2015-02-02 14:42:44 -0600

    Editing

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,11 +2,13 @@ class Appointment < ActiveRecord::Base
     validates_presence_of :first_name, :last_name, :age,
     :date_of_birth, :city, :state, :zip_code, :urgency, :appt_time
 
-    # validates :age, numericality: { greater_than: 20 }
+    validates :age, numericality: { greater_than: 20 }
 
-    validates :age, -> { where(arel_table[:age] >= 21), " MUST BE GREATER"}
+    #validates :age, -> { where(arel_table[:age] >= 21)}
 
 
+
+  scope :long_name, -> { where("char_length('contacts.last_name') < 10") }
 
 end
 
