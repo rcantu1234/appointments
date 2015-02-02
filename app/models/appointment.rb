bd0944d847df3763ca6da244b4d1a7543fd95c17
ROEL CANTU <roel.cantu@yahoo.com>
2015-02-02 17:04:18 -0600

    Editing

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -5,9 +5,6 @@ class Appointment < ActiveRecord::Base
     validates :age, numericality: { greater_than: 20 }
     #validates :age, -> { where(arel_table[:age] >= 21)}
 
-f.select :urgency, options_for_select(%w[High Low None])
-and then you use the same (%w[High Low None])
-
   #scope :long_name, -> { where("char_length('contacts.last_name') < 10") }
 
 end
