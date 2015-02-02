ac8edc0c7f2976289ab3c8654b2c85caf39efe5e
ROEL CANTU <roel.cantu@yahoo.com>
2015-02-02 17:24:58 -0600

    Editing

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,11 +1,14 @@
 class Appointment < ActiveRecord::Base
+
+ belongs_to :category
     validates_presence_of :first_name, :last_name, :age,
     :date_of_birth, :city, :state, :zip_code, :urgency, :appt_time
 
-    validates :age, numericality: { greater_than: 20 }
-    #validates :age, -> { where(arel_table[:age] >= 21)}
+    validates :age, numericality: { greater_than: 21 }
 
-  #scope :long_name, -> { where("char_length('contacts.last_name') < 10") }
+    validates_uniqueness_of :age, scope: :appt_time, message: " there is already an appointment."
+
+    scope :age, where(:age => true)
 
 end
 
