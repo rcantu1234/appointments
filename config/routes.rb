518e9be3ca3cbdedfbcb56294f67e53b32620755
ROEL CANTU <roel.cantu@yahoo.com>
2015-02-02 21:50:26 -0600

    Editing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-
-  root 'admin#index'
-  resources :appointments, :path => "admin"
+  resources :appointments
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
