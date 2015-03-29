03f3d24d2f3dfe02df46380441d1b45b432886dd
ROEL CANTU <roel.cantu@yahoo.com>
2015-03-29 09:12:18 -0500

    Changed validation for age

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,13 +3,13 @@ class Appointment < ActiveRecord::Base
     validates_presence_of :first_name, :last_name, :age,
     :date_of_birth, :city, :state, :zip_code, :urgency, :appt_time
 
-    validates :age, numericality: { greater_than: 21 }
+    validates :date_of_birth
 
     validates :first_name, :uniqueness => { :message => " has been used."}
 
     validates :appt_time, :uniqueness => { :message => " has been taken." }
 
-    # validates_length_of :zip_code.length, :maximum => 5, :allow_blank => false
+    validates :zip_code, length: { minimum: 5, maximum: 5 }, allow_blank:  false
 
 end
 
