a76795b1be942be2db0436d5d009d602af5ba026
ROEL CANTU <roel.cantu@yahoo.com>
2015-03-29 22:05:12 -0500

    Validating date of birth and zip code

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,13 +3,15 @@ class Appointment < ActiveRecord::Base
     validates_presence_of :first_name, :last_name, :age,
     :date_of_birth, :city, :state, :zip_code, :urgency, :appt_time
 
-    validates :date_of_birth
+    validates_inclusion_of :date_of_birth, :in => Date.new(1994)..Time.now.years_ago(21).to_date,
+    :message => 'You must be 21 years or older'
 
-    validates :first_name, :uniqueness => { :message => " has been used."}
+    #validates :first_name, :uniqueness => { :message => " has been used."}
 
     validates :appt_time, :uniqueness => { :message => " has been taken." }
 
-    validates :zip_code, length: { minimum: 5, maximum: 5 }, allow_blank:  false
+    validates :zip_code, presence: true
+    validates_length_of :zip_code, minimum: 5, maximum: 5
 
 end
 
